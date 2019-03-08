f53337e2dca0e42744bc52aaea53d1aef24ee966
Mark Miller <constenum@gmail.com>
2019-03-07 21:30:57 -0500

    Update README file; added index file with an image.

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>DWA Practice</title>
+</head>
+<body>
+  <h1>Hello World!</h1>
+  <img src='/images/black-white-city-street.jpg' alt='City Street - black and white'>
+</body>
+</html>
